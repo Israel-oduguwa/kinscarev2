4d3bc914d8aa9764d0419c515cd8268409e88d84
Israel-oduguwa <adeboyega22@gmail.com>
2026-01-05 14:04:01 +0100

    Fixed payment issues

--- src/Admin/Blog/ImageUploader.tsx
+++ src/Admin/Blog/ImageUploader.tsx
@@ -68,7 +68,7 @@ export default function ImageUploader({
   const handleDelete = async () => {
     try {
       await axios.post(
-        "http://localhost:8081/api/v1/delete-file",
+        "https://jrp7pe2xhj.us-east-1.awsapprunner.com/api/v1/delete-file",
         {
           fileUrl: value,
         }
